5849829def69a1ae193f146688f14d65bc596495
Dinar <dinar-97@mail.ru>
2024-01-19 16:52:53 +0300

    add api tests

--- src/test/java/RegApiTests.java
+++ src/test/java/RegApiTests.java
@@ -3,8 +3,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
 
 
 public class RegApiTests {
@@ -30,7 +32,14 @@ public class RegApiTests {
                 .post("https://reqres.in/api/users")
 
                 .then()
-                .statusCode(201);
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"),
+                        "job", is("leader"))
+                .extract().response();
+
+
     }
     @Test
     @Story("Негативные тесты")
@@ -46,9 +55,15 @@ public class RegApiTests {
                 .log().method()
                 .log().uri()
                 .log().body()
+
+                .when()
                 .post("https://reqres.in/api/users")
+
                 .then()
-                .statusCode(400);
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .extract().response();
     }
     @Test
     @Story("Позитивные тесты")
@@ -66,8 +81,38 @@ public class RegApiTests {
                 .log().method()
                 .log().uri()
                 .log().body()
+
+                .when()
                 .put("https://reqres.in/api/users/2")
+
                 .then()
-                .statusCode(200);
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response();
+    }
+    @Test
+    @Story("Негативные тесты")
+    @Tag("negative")
+    @DisplayName("Регистрация без \"password\"")
+    void unsuccessfulLoginUserTest(){
+        String regData = "{\n"+"\"email\": \"peter@klaven\"}";
+
+        given()
+                .body(regData)
+                .contentType(JSON)
+                .log().method()
+                .log().uri()
+                .log().body()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error",is("Missing password"))
+                .extract().response();
     }
 }
